aa67abdc6180baaf4824846f6321ee2e3fe03e06
Jacob Seppanen <jbseppanen@gmail.com>
2019-01-29 15:46:41 -0800

    Add navigation drawer menu and header xml files.

--- app/src/main/java/com/example/androidmenusdesign/MainActivity.java
+++ app/src/main/java/com/example/androidmenusdesign/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.androidmenusdesign;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
