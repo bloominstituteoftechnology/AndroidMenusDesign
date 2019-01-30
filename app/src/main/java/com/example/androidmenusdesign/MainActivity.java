ba534bedbd58cbf248fcc1383893f52cfc741dce
Jacob Seppanen <jbseppanen@gmail.com>
2019-01-29 20:16:48 -0800

    Get theme switch to work.

--- app/src/main/java/com/example/androidmenusdesign/MainActivity.java
+++ app/src/main/java/com/example/androidmenusdesign/MainActivity.java
@@ -9,10 +9,14 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SwitchCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -30,28 +34,38 @@ public class MainActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
         drawerLayout = findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+        final ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+        MenuItem item = navigationView.getMenu().findItem(R.id.nav_night_mode);
+        View rootView = item.getActionView();
+        SwitchCompat themeSwitch = rootView.findViewById(R.id.drawer_switch);
+        themeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                Toast.makeText(context, menuItem.getTitle(), Toast.LENGTH_LONG).show();
-                menuItem.setChecked(!menuItem.isChecked());
-                drawerLayout.closeDrawers();
-
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    if (menuItem.getItemId() == R.id.nav_night_mode) {
-                        final UiModeManager uiModeManager = context.getSystemService(UiModeManager.class);
-                        uiModeManager.setNightMode(menuItem.isChecked() ? UiModeManager.MODE_NIGHT_YES : UiModeManager.MODE_NIGHT_NO);
-
-                    }
+                    final UiModeManager uiModeManager = context.getSystemService(UiModeManager.class);
+                    uiModeManager.setNightMode(isChecked ? UiModeManager.MODE_NIGHT_YES : UiModeManager.MODE_NIGHT_NO);
                 }
 
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull final MenuItem menuItem) {
+                Toast.makeText(context, menuItem.getTitle(), Toast.LENGTH_LONG).show();
+                menuItem.setChecked(!menuItem.isChecked());
+                if (menuItem.getItemId() == R.id.nav_night_mode) {
+                    return false;
+                }
+                drawerLayout.closeDrawers();
                 return true;
             }
         });
@@ -61,8 +75,6 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_activity_options_menu, menu);
-
-
         return true;
     }
 }
