01e274f19aba4b838befb13ffa56617130f39abb
Jacob Seppanen <jbseppanen@gmail.com>
2019-01-29 19:18:00 -0800

    Get options menu to display.

--- app/src/main/java/com/example/androidmenusdesign/MainActivity.java
+++ app/src/main/java/com/example/androidmenusdesign/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.androidmenusdesign;
 
+import android.app.UiModeManager;
 import android.content.Context;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
@@ -8,21 +10,27 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
     Context context;
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        context = this;
+
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        drawerLayout = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
@@ -32,9 +40,29 @@ public class MainActivity extends AppCompatActivity {
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Toast.makeText(context, menuItem.getTitle(), Toast.LENGTH_LONG).show();
+                menuItem.setChecked(!menuItem.isChecked());
+                drawerLayout.closeDrawers();
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (menuItem.getItemId() == R.id.nav_night_mode) {
+                        final UiModeManager uiModeManager = context.getSystemService(UiModeManager.class);
+                        uiModeManager.setNightMode(menuItem.isChecked() ? UiModeManager.MODE_NIGHT_YES : UiModeManager.MODE_NIGHT_NO);
+
+                    }
+                }
+
                 return true;
             }
         });
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_options_menu, menu);
+
+
+        return true;
     }
 }
