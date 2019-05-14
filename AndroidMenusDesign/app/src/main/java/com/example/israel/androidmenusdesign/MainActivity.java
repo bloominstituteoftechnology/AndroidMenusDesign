f5f8e1ae60b18cae04843cb393ea063a59fd3fa7
Israel dela Cruz <igdcruz42@gmail.com>
2019-05-14 17:18:13 -0500

    toolbar

--- AndroidMenusDesign/app/src/main/java/com/example/israel/androidmenusdesign/MainActivity.java
+++ AndroidMenusDesign/app/src/main/java/com/example/israel/androidmenusdesign/MainActivity.java
@@ -4,10 +4,11 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
@@ -19,9 +20,18 @@ implements NavigationView.OnNavigationItemSelectedListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        NavigationView navigationView = findViewById(R.id.activitiy_main_nav);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        DrawerLayout drawer = findViewById(R.id.activity_main_drawer_root);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.activitiy_main_nav);
         navigationView.setNavigationItemSelectedListener(this);
+
     }
 
     @Override
@@ -37,6 +47,24 @@ implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        onMenuItemSelected(menuItem);
+
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.nav_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        onMenuItemSelected(item);
+        return true;
+    }
+
+    private void onMenuItemSelected(MenuItem menuItem) {
         int id = menuItem.getItemId();
 
         switch (id) {
@@ -53,7 +81,5 @@ implements NavigationView.OnNavigationItemSelectedListener {
                 toast.show();
             } break;
         }
-
-        return true;
     }
 }
