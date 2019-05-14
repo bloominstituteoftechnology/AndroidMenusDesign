fb98d8af6c687334c76320d617a1c656e6d548f2
KyleCole01 <kyle@misfitselectronics.com>
2019-05-14 17:06:59 -0400

    changed button to launch list activity to image button and used random image for now.

--- app/src/main/java/com/example/androidmenusdesign/MainActivity.java
+++ app/src/main/java/com/example/androidmenusdesign/MainActivity.java
@@ -6,9 +6,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-Button button;
+ImageButton button;
 Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +17,14 @@ Context context;
         setContentView(R.layout.activity_main);
         context = this;
         button = findViewById(R.id.button_recipelist);
+        Recipe testRecipe = new Recipe(2,"cookie","dessert", 3,4,2,"test ingredient List \n cookie dough\n oven","make and bake");
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(context, RecipeListActivity.class);
                 startActivity(intent);
+//                public Recipe(String name, String category, int starRating, int feedPerBatch,int costRating,String ingredientList,String directions)
+
             }
         });
     }
--- app/src/main/java/com/example/androidmenusdesign/Recipe.java
+++ app/src/main/java/com/example/androidmenusdesign/Recipe.java
@@ -1,4 +1,73 @@
 package com.example.androidmenusdesign;
 
 public class Recipe {
+    private int id, starRating, costRating, feedPerBatch;
+    private String name, directions, ingredientList,category;
+
+    public Recipe(int id,String name, String category, int starRating, int feedPerBatch,int costRating,String ingredientList,String directions) {
+        this.starRating = starRating;
+        this.costRating = costRating;
+        this.feedPerBatch = feedPerBatch;
+        this.name = name;
+        this.directions = directions;
+        this.ingredientList = ingredientList;
+        this.category = category;
+        this.id = id;
+    }
+
+    public int getStarRating() {
+        return starRating;
+    }
+
+    public void setStarRating(int starRating) {
+        this.starRating = starRating;
+    }
+
+    public int getCostRating() {
+        return costRating;
+    }
+
+    public void setCostRating(int costRating) {
+        this.costRating = costRating;
+    }
+
+    public int getFeedPerBatch() {
+        return feedPerBatch;
+    }
+
+    public void setFeedPerBatch(int feedPerBatch) {
+        this.feedPerBatch = feedPerBatch;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDirections() {
+        return directions;
+    }
+
+    public void setDirections(String directions) {
+        this.directions = directions;
+    }
+
+    public String getIngredientList() {
+        return ingredientList;
+    }
+
+    public void setIngredientList(String ingredientList) {
+        this.ingredientList = ingredientList;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
