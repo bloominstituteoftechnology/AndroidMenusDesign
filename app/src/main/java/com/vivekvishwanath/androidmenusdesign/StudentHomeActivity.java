d88729bcfc4599497c79c83cd18331d0b2d73e53
VivekV95 <vivekv210@gmail.com>
2019-05-14 17:26:13 -0500

    Modified default drawer to suit project week reqs

--- app/src/main/java/com/vivekvishwanath/androidmenusdesign/StudentHomeActivity.java
+++ app/src/main/java/com/vivekvishwanath/androidmenusdesign/StudentHomeActivity.java
@@ -14,13 +14,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 
-public class MainActivity extends AppCompatActivity
+public class StudentHomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_student_home);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
